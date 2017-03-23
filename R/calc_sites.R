fc171686c490208578c099b9a49c11969e012d8c
MiKatt <kattwinkel-mira@uni-landau.de>
2017-03-23 17:07:25 +0100

    added prediction_sites and predictor maps to import_data and calc_sites

--- R/calc_sites.R
+++ R/calc_sites.R
@@ -6,6 +6,9 @@
 #'@description A vector (points) map 'sites' is derived and several attributes
 #'are assigned.
 #'
+#'@param pred_sites character vector (optional); names for prediction sites 
+#'(created with \code{import_data}).
+#'
 #'@details Steps include:
 #'\itemize{
 #'\item{Snap points to derived network. 'dist'
@@ -25,6 +28,11 @@
 #'
 #''pid' and 'locID' are identical, unique numbers. 'upDist' is calculated using
 #'\href{https://grass.osgeo.org/grass73/manuals/r.stream.distance.html}{r.stream.distance}.
+#'
+#'If prediction sites have been created outside of this packeage they can be 
+#'processed here as well. They must have been imported with \code{\link{import_data}}
+#'before. Alternatively, prediction sites can be created using
+#'\code{\link{calc_prediction_sites}}.
 #'
 #'@note \code{\link{import_data}}, \code{\link{derive_streams}} and
 #'  \code{\link{calc_edges}} must be run before.
@@ -60,7 +68,7 @@
 #' points(sites, pch = 4)
 #' }
 
-calc_sites <- function() {
+calc_sites <- function(pred_sites = NULL) {
   vect <- execGRASS("g.list",
                     parameters = list(
                       type = "vect"
@@ -75,24 +83,49 @@ calc_sites <- function() {
     stop("Sites not found. Did you run import_data()?")
   if (!"edges" %in% vect)
     stop("Edges not found. Did you run calc_edges()?")
+  
+  if(!is.null(pred_sites)){
+   i <- grep("_o$",pred_sites)
+   pred_sites[-i] <- paste0(pred_sites[-i],"_o")
+   if (any(!pred_sites %in% vect))
+     stop("Prediction sites not found. Did you run import_data() on them?")
+  }
+  
+  site_maps <- c("sites", pred_sites)
+  site_maps <- sub("_o$","", site_maps)
+  sapply(site_maps, prepare_sites)
+  
+}
+
+#' Snap sites to streams and calculate attributes
+#' @param sites_map character; name of sites map (observation or prediction sites)
+#' as created by \code{import_data}.
+#' 
+#' @details 
+#' This function is called by \code{calc_sites} and should not be called directly.
+#' Sites are snapped to the streams and upstream distance is calculated.
+#' Sites are snapped to the streams and upstream distance is calculated.
+#'
+
+prepare_sites <- function(sites_map){
   execGRASS("g.copy",
             flags = c("overwrite", "quiet"),
             parameters = list(
-              vector = "sites_o,sites"))
-
+              vector = paste0(paste0(sites_map,"_o"), ",",sites_map)))
+  
   # Snap sites to streams --------
   message("Snapping sites to streams...\n")
   # add 4 columns holding: stream, distance and coordinates of nearest streams
   execGRASS("v.db.addcolumn",
             parameters = list(
-              map = "sites",
+              map = sites_map,
               columns = "cat_edge int,dist double precision,xm double precision,ym double precision"
             ))
   # calc distance
   # MiKatt: additionally get cat of nearest edge for later joining of netID and rid
   execGRASS("v.distance",
             flags = c("overwrite", "quiet"),
-            parameters = list(from = "sites",
+            parameters = list(from = sites_map,
                               to = "edges",
                               #output = "connectors",
                               upload = "cat,dist,to_x,to_y",
@@ -100,20 +133,20 @@ calc_sites <- function() {
   #! This is in R faster than in GRASS!? (which has to write to hard-drive)
   #! Other possibilities in GRASS to change coordinates?
   #! use r.stream.snap alternatively?
-  sites <- readVECT("sites", type = "point", ignore.stderr = TRUE)
+  sites <- readVECT(sites_map, type = "point", ignore.stderr = TRUE)
   proj4 <- proj4string(sites)
   sites <-  as(sites, "data.frame")
   coordinates(sites) <-  ~ xm + ym
   proj4string(sites) <- proj4
   names(sites)[names(sites) %in% c( "coords.x1", "coords.x2")] <- c("xm", "ym")
   sites$cat_ <- NULL
-  writeVECT(sites, vname = "sites",
+  writeVECT(sites, vname = sites_map,
             v.in.ogr_flags = c("overwrite", "quiet"),
             ignore.stderr = TRUE)
   nsites <- nrow(sites)
   sites.coor <- data.table(sites$cat,sites$xm,sites$ym)
   rm(sites)
-
+  
   ### Setting pid -----------
   #! pid and locID identical???
   # MiKatt: pid is for "repeated measurements at a singel location" (Peterson & Ver Hoef, 2014: Stars: An ArcGIS Toolset Used to Calculate the Spatial Information Neede to Fit SPatial Statistical Models to Stream Network Data, p. 13)
@@ -121,34 +154,34 @@ calc_sites <- function() {
   # MiKatt: Also keep user defined site IDs / site names
   message("Setting pid and locID...\n")
   execGRASS("v.db.addcolumn",
-            parameters = list(map = "sites",
+            parameters = list(map = sites_map,
                               columns = "pid int,locID int"))
   execGRASS("v.db.update",
-            parameters = list(map = "sites",
+            parameters = list(map = sites_map,
                               column = "pid",
                               value = "cat"))
   execGRASS("v.db.update",
-            parameters = list(map = "sites",
+            parameters = list(map = sites_map,
                               column = "locID",
                               value = "pid"))
-
+  
   # Set netID and rid from network ---------
   message("Assigning netID and rid...\n")
-
+  
   # MiKatt: This seems to be faster
   execGRASS("v.db.addcolumn",
             flags = c("quiet"),
-            parameters = list(map = "sites",
+            parameters = list(map = sites_map,
                               columns = "netID int, rid int"))
   execGRASS("db.execute",
             parameters = list(
-              sql="UPDATE sites SET rid=(SELECT rid FROM edges WHERE sites.cat_edge=edges.cat)"
+              sql=paste0('UPDATE ', sites_map, ' SET rid=(SELECT rid FROM edges WHERE ', sites_map, '.cat_edge=edges.cat)')
             ))
   execGRASS("db.execute",
             parameters = list(
-              sql="UPDATE sites SET netID=(SELECT netID FROM edges WHERE sites.cat_edge=edges.cat)"
+              sql=paste0('UPDATE ', sites_map, ' SET netID=(SELECT netID FROM edges WHERE ', sites_map, '.cat_edge=edges.cat)')
             ))
-
+  
   # Calculate upDist ---------
   # MiKatt: Distance of every raster cell from the outlet
   message("Calculating upDist...\n")
@@ -161,33 +194,33 @@ calc_sites <- function() {
               distance = "upDist"
             ))
   execGRASS("v.db.addcolumn",
-            parameters = list(map = "sites",
+            parameters = list(map = sites_map,
                               columns = "upDist double"))
   execGRASS("v.what.rast",
             flags = c("quiet"),
             parameters = list(
-              map = "sites",
+              map = sites_map,
               raster = "upDist",
               column = "upDist"
             ))
   execGRASS("v.db.update", flags = c("quiet"),
             parameters = list(
-              map = "sites",
+              map = sites_map,
               column = "upDist",
               value = "round(upDist, 2)"
             ))
-
+  
   # Calculate distRatio = distance from lower end of edge to site / length edge
   message("Calculating distance ratio...\n")
   execGRASS("v.db.addcolumn",
             flags = c("quiet"),
             parameters = list(
-              map = "sites",
+              map = sites_map,
               columns = "distRatio double precision"
             ))
-  sql_str <- paste0("UPDATE sites SET distRatio=1-",
-                    "round((((SELECT upDist FROM edges WHERE edges.cat=sites.cat_edge)-upDist)),2)/",
-                    "(SELECT Length FROM edges WHERE edges.cat=sites.cat_edge)")
+  sql_str <- paste0('UPDATE ', sites_map, ' SET distRatio=1-',
+                    'round((((SELECT upDist FROM edges WHERE edges.cat=', sites_map, '.cat_edge)-upDist)),2)/',
+                    '(SELECT Length FROM edges WHERE edges.cat=', sites_map, '.cat_edge)')
   execGRASS("db.execute",
             parameters = list(
               sql=sql_str
--- R/import_data.R
+++ R/import_data.R
@@ -1,8 +1,8 @@
 #' Import data into GRASS.
 #'
 #' This function loads dem and sites data (both required) into the GRASS session.
-#' Optionally, streams data is loaded and the streams may be corrected by
-#' snapping to prevent lose ends.
+#' Optionally, prediction sites and streams data can be loaded and the streams 
+#' may be corrected by snapping to prevent lose ends.
 #'
 #' @param dem character; path to DEM raster file.
 #' @param sites character; path to sites vector file.
@@ -11,15 +11,21 @@
 #' @param snap_streams boolean (optional); snap line ends.
 #'  If TRUE line ends of the streams are snapped to the next feature if they are
 #'   unconncted with threshold of 10 m using GRASS function v.clean.
-#' @param ... other paths to raster data to import as predictors
-#'  (not yet implemented).
+#' @param pred_sites character vector (optional); paths to prediction sites 
+#' vector files
+#' @param predictor_maps character vector (optional); paths to raster data to 
+#' import as predictors.
 #'
 #' @return Nothing, the data is loaded into the GRASS session.
-#' The DEM is stored as raster 'dem', sites as vector 'sites_o' and streams as
-#' vector 'streams_o' in the GRASS location (mapset PERMANENT).
+#' The DEM is stored as raster 'dem', sites as vector 'sites_o', prediction sites
+#' vector(s) using the original file names with an appended '_o' (without extension),
+#' predictor map raster(s)  using the original file names (without extension) 
+#' and streams as vector 'streams_o' in the GRASS location 
+#' (mapset PERMANENT).
 #'
 #' @note A GRASS session must be initiated before, see \code{\link[rgrass7]{initGRASS}}.
-#' @author Eduard Szoecs, \email{eduardszoecs@@gmail.com}
+#' @author Eduard Szoecs, \email{eduardszoecs@@gmail.com},  Mira Kattwinkel
+#'  \email{mira.kattwinkel@@gmx.net}
 #' @export
 #'
 #' @examples
@@ -37,7 +43,8 @@
 #' }
 #'
 
-import_data <- function(dem, sites, streams = NULL, snap_streams = FALSE, ...) {
+import_data <- function(dem, sites, streams = NULL, snap_streams = FALSE, 
+                        pred_sites = NULL, predictor_maps = NULL) {
   if (nchar(get.GIS_LOCK()) == 0)
     stop("GRASS not initialised. Please run initGRASS().")
   if (is.null(dem) | is.null(sites))
@@ -63,16 +70,60 @@ import_data <- function(dem, sites, streams = NULL, snap_streams = FALSE, ...) {
                 input = dem,
                 output = "dem"),ignore.stderr=T)
   }
-  message("Loading sites into GRASS...\n")
+  
+  message("Loading sites into GRASS as sites_o ...\n")
   # sites data
   execGRASS("v.in.ogr",
             flags = c("o", "overwrite", "quiet"),
             parameters = list(
               input = sites,
               output = "sites_o"),ignore.stderr=T)
+  
+  
+  # prediction sites data
+  if (!is.null(pred_sites)) {
+    pred_sites_names <- do.call(rbind,base::strsplit(sapply(pred_sites,basename,USE.NAMES=F), split="[.]"))[,1]
+    if(any(pred_sites_names == "sites")){
+      message("Prediction sites cannot be named 'sites'. Please rename.")
+    } else{
+      pred_sites_names <- paste0(pred_sites_names, "_o")
+      message(paste0("Loading preditions sites into GRASS as ",
+                     paste(pred_sites_names, collapse=", ", sep=""), " ...\n"))
+      for(i in 1:length(pred_sites)){
+        execGRASS("v.in.ogr",
+                  flags = c("o", "overwrite", "quiet"),
+                  parameters = list(
+                    input = pred_sites[i],
+                    output = pred_sites_names[i]),ignore.stderr=T)
+      }
+    }
+  }
+  
 
+  # predictor raster maps
+  if (!is.null(predictor_maps)) {
+    pred_map_names <- do.call(rbind,base::strsplit(sapply(predictor_maps,basename,USE.NAMES=F), split="[.]"))[,1]
+    message(paste0("Loading predictior varibales into GRASS as ",paste(pred_map_names,collapse=", ", sep=""), " ...\n"))
+    for(i in 1:length(pred_map_names)){
+      if(.Platform$OS.type == "windows"){
+        execGRASS("r.in.gdal",
+                  flags = c("overwrite","quiet","o"),
+                  parameters = list(
+                    input = predictor_maps[i],
+                    output = pred_map_names[i]),ignore.stderr=T)
+      } else{
+        execGRASS("r.in.gdal",
+                  flags = c("overwrite", "quiet"),
+                  parameters = list(
+                    input = predictor_maps[i],
+                    output =pred_map_names[i]),ignore.stderr=T)
+      }
+    }
+  }
+  
   # streams data
   if (!is.null(streams)) {
+    message("Loading streams into GRASS as streams_o  ...\n")
     execGRASS("v.in.ogr",
               flags = c("overwrite", "quiet"),
               parameters = list(
