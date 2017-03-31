f9d56e105ddd814fc492f323983596bdec108a0d
MiKatt <kattwinkel-mira@uni-landau.de>
2017-03-31 16:28:48 +0200

    corrected calc_attributes* for NA values for yes/no maps

--- R/calc_attributes_edges.R
+++ R/calc_attributes_edges.R
@@ -138,8 +138,10 @@ calc_attributes_edges <- function(input_raster, stat, attr_name, round_dig = 2,
 
     if(stat[j] %in% names(st)){
       st <- st[, c("zone", stat[j]), with = FALSE]
-    } else { # if(stat == "percent")
-      st <- st[, c("zone", "mean"), with = FALSE]  # if e.g. coded as 0 and 1, mean gives the ratio
+    } else if(any(st[,variance] != 0)) { # if(stat == "percent") and coded as 0 and 1, mean gives the ratio
+      st <- st[, c("zone", "mean"), with = FALSE]  
+    } else{  # if coded as something and NA, null(), no data value
+       st[, non_null_cells/(non_null_cells + null_cells), by = zone]
     }
     names(st)[2] <- attr_name[j]
     st[, attr_name[j] := round(st[, attr_name[j], with = FALSE], round_dig[j])]
