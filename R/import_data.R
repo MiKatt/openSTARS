5cf1d4703dc03f30e3b7214c7e7686065cef7dc2
MiKatt <kattwinkel-mira@uni-landau.de>
2018-02-18 16:40:24 +0100

    setup_environment uses only dem, v.import in import_data

--- R/import_data.R
+++ R/import_data.R
@@ -4,33 +4,38 @@
 #' Optionally, prediction sites and streams data can be loaded and the streams 
 #' may be corrected by snapping to prevent lose ends.
 #'
-#' @param dem character; path to DEM raster file.
+#' @param dem character; path to DEM (digital elevation model) raster file.
 #' @param band integer (optional); defines which band is used
 #' @param sites character string or object; path to sites vector file (shape) 
 #' or sp data object.
 #' @param streams character string or object (optional); path to network vector 
-#' file (shape) or sp data object.
-#'  If available it can be burnt into DEM.
+#' file (shape) or sp data object. If available this can be burnt into the DEM 
+#' in \code{\link{derive_streams}}
 #' @param snap_streams boolean (optional); snap line ends.
 #'  If TRUE line ends of the streams are snapped to the next feature if they are
 #'   unconnected with threshold of 10 m using 'GRASS' function v.clean.
 #' @param pred_sites character vector (optional); paths to prediction sites 
 #' vector files
-#' @param predictor_maps character vector (optional); paths to raster data to 
+#' @param predictor_raster character vector (optional); paths to raster data to 
 #' import as predictors.
-#' @param predictor_names character vector (optional); names for potential predictor
+#' @param predictor_r_names character vector (optional); names for potential predictor
 #' variables.
 #'
 #' @return Nothing, the data is loaded into the 'GRASS' session (mapset PERMANENT).
 #' The DEM is stored as raster 'dem', sites as vector 'sites_o', prediction sites
 #' vector(s) using the original file names with an appended '_o' (without extension),
 #' streams as vector 'streams_o' in the 'GRASS' location. Additionally, predictor 
-#' map raster(s) can be read in and are stored in 'GRASS' using either the 
+#' raster map(s) can be read in and are stored in 'GRASS' using either the 
 #' original file names (without extension) or using the names provides in 
-#' predictor_names. The latter option may be useful if ArcGIS grid data 
+#' predictor_r_names. The latter option may be useful if ArcGIS grid data 
 #' (typically stored as 'grid_name/w001001.adf') are used.
-#'.
-#'
+#' 
+#' @details All vector data (sites and streams) is imported into the current
+#' location using \href{https://grass.osgeo.org/grass74/manuals/v.import.html}{v.import}.
+#' Hence, if the projections does not match to the one of the dem (which was used
+#' to specify the location in \code{\link{setup_grass_environment}}) the maps 
+#' are imported on the fly.
+#' 
 #' @note A GRASS session must be initiated before, see \code{\link[rgrass7]{initGRASS}}.
 #' @author Eduard Szoecs, \email{eduardszoecs@@gmail.com},  Mira Kattwinkel
 #'  \email{mira.kattwinkel@@gmx.net}
@@ -51,7 +56,7 @@
 # Load files into GRASS
 #' dem_path <- system.file("extdata", "nc", "elev_ned_30m.tif", package = "openSTARS")
 #' sites_path <- system.file("extdata", "nc", "sites_nc.shp", package = "openSTARS")
-#' setup_grass_environment(dem = dem_path, sites = sites_path)
+#' setup_grass_environment(dem = dem_path)
 #' import_data(dem = dem_path, sites = sites_path)
 #' gmeta()
 #' dem <- readRAST('dem')
@@ -59,51 +64,63 @@
 #' }
 
 import_data <- function(dem, band = 1, sites, streams = NULL, snap_streams = FALSE, 
-                        pred_sites = NULL, predictor_maps = NULL, predictor_names = NULL) {
+                        pred_sites = NULL, predictor_raster = NULL, predictor_r_names = NULL) {
   if (nchar(get.GIS_LOCK()) == 0)
     stop("GRASS not initialised. Please run initGRASS().")
   if (is.null(dem) | is.null(sites))
     stop("DEM and sites are needed.")
-
+  
   # Import data -------------------
-  message("Loading DEM into GRASS...")
-
-  # import raster with correct extent
+  message("Loading DEM into GRASS...\n")
+  
+  # import raster
   # MiKatt: it is necassary to set the region with g.region in setup_grass_environment;
   #         using flag "e" when importing the dem does not work
   #         (r.hydrodem produces very huge raster)
-  if(.Platform$OS.type == "windows"){
-    execGRASS("r.in.gdal",
-              flags = c("overwrite","quiet","o"),
-              parameters = list(
-                input = dem,
-                band = band,
-                output = "dem"),ignore.stderr=T)
-  } else{
-    execGRASS("r.in.gdal",
-              flags = c("overwrite", "quiet"),
-              parameters = list(
-                input = dem,
-                band = band,
-                output = "dem"),ignore.stderr=T)
-  }
+  # 20180215: not necessary?
+  # if(.Platform$OS.type == "windows"){
+  #   execGRASS("r.in.gdal",
+  #             flags = c("overwrite","quiet","o"),
+  #             parameters = list(
+  #               input = dem,
+  #               band = band,
+  #               output = "dem"),ignore.stderr=T)
+  # } else{
+  execGRASS("r.in.gdal",
+            flags = c("overwrite", "quiet"),
+            parameters = list(
+              input = dem,
+              band = band,
+              output = "dem"),ignore.stderr=T)
+  #}
   
-  message("Loading sites into GRASS as sites_o...")
+  message("Loading sites into GRASS as sites_o ...\n")
   # sites data
   # flag "-r": only current region
+  # 20180216: not flag "o", because if projection is wrong I want to see an error
   if(inherits(sites, 'Spatial')) {
-    writeVECT(sites, "sites_o",  v.in.ogr_flags = c("o", "overwrite", "quiet", "r"),
+    writeVECT(sites, "sites_o",  v.in.ogr_flags = c("overwrite", "quiet", "r"),
               ignore.stderr=T)
-  #} # MiKatt: exclude as long as under development
-  # else if(inherits(sites, 'sf')) { 
-  #   sites_sp <- as(sites, 'Spatial') # AS:  no method for sf yet imho
-  #   writeVECT(sites_sp, "sites_o", v.in.ogr_flags = c("o", "overwrite", "quiet"),
-  #             ignore.stderr=T)
+    #} # MiKatt: exclude as long as under development
+    # else if(inherits(sites, 'sf')) { 
+    #   sites_sp <- as(sites, 'Spatial') # AS:  no method for sf yet imho
+    #   writeVECT(sites_sp, "sites_o", v.in.ogr_flags = c("o", "overwrite", "quiet"),
+    #             ignore.stderr=T)
   } else {
-  execGRASS("v.in.ogr", flags = c("o", "overwrite", "quiet", "r"),
-            parameters = list(
-              input = sites,
-              output = "sites_o"),ignore.stderr=T)
+    # a <- execGRASS("v.in.ogr", flags = c("j"),
+    #           parameters = list(
+    #             input = sites,
+    #             output = "sites_o"), intern = T)
+    # 
+    # execGRASS("v.in.ogr", flags = c("overwrite", "quiet", "r"), #"o", 
+    #           parameters = list(
+    #             input = sites,
+    #             output = "sites_o"),ignore.stderr=T)
+    execGRASS("v.import", flags = c("overwrite", "quiet"),
+              parameters = list(
+                input = sites,
+                output = "sites_o",
+                extent = "region"))
   }
   
   # prediction sites data
@@ -114,54 +131,66 @@ import_data <- function(dem, band = 1, sites, streams = NULL, snap_streams = FAL
     } else{
       pred_sites_names <- paste0(pred_sites_names, "_o")
       message(paste0("Loading preditions sites into GRASS as ",
-                     paste(pred_sites_names, collapse=", ", sep=""), "..."))
+                     paste(pred_sites_names, collapse=", ", sep=""), " ...\n"))
       for(i in 1:length(pred_sites)){
-        execGRASS("v.in.ogr",
-                  flags = c("o", "overwrite", "quiet"),
+        # execGRASS("v.in.ogr",
+        #           flags = c("o", "overwrite", "quiet"),
+        #           parameters = list(
+        #             input = pred_sites[i],
+        #             output = pred_sites_names[i]),ignore.stderr=T)
+        execGRASS("v.import",
+                  flags = c("overwrite", "quiet"),
                   parameters = list(
                     input = pred_sites[i],
-                    output = pred_sites_names[i]),ignore.stderr=T)
+                    output = pred_sites_names[i],
+                    extent = "region"))
       }
     }
   }
   
-
+  
   # predictor raster maps
-  if (!is.null(predictor_maps)) {
-    if(is.null(predictor_names))
-      predictor_names <- do.call(rbind,base::strsplit(sapply(predictor_maps,basename,USE.NAMES=F), split="[.]"))[,1]
-    message(paste0("Loading predictior varibales into GRASS as ",paste(predictor_names, collapse = ", ", sep=""), "..."))
-    for(i in 1:length(predictor_names)){
+  if (!is.null(predictor_raster)) {
+    if(is.null(predictor_r_names))
+      predictor_r_names <- do.call(rbind,base::strsplit(sapply(predictor_raster,basename,USE.NAMES=F), split="[.]"))[,1]
+    message(paste0("Loading predictior varibales into GRASS as ",paste(predictor_r_names, collapse = ", ", sep=""), " ...\n"))
+    for(i in 1:length(predictor_r_names)){
       if(.Platform$OS.type == "windows"){
         execGRASS("r.in.gdal",
                   flags = c("overwrite","quiet","o"),
                   parameters = list(
-                    input = predictor_maps[i],
-                    output = predictor_names[i]),ignore.stderr=T)
+                    input = predictor_raster[i],
+                    output = predictor_r_names[i]),ignore.stderr=T)
       } else{
         execGRASS("r.in.gdal",
                   flags = c("overwrite", "quiet"),
                   parameters = list(
-                    input = predictor_maps[i],
-                    output = predictor_names[i]),ignore.stderr=T)
+                    input = predictor_raster[i],
+                    output = predictor_r_names[i]),ignore.stderr=T)
       }
     }
   }
   
   # streams data
   if (!is.null(streams)) {
-    message("Loading streams into GRASS as streams_o...")
+    message("Loading streams into GRASS as streams_o  ...\n")
     # flag "-r": only current region
-
+    
     if(inherits(streams, 'Spatial')) {
       writeVECT(streams, "streams_o",  v.in.ogr_flags = c("o", "overwrite", "quiet", "r"),
                 ignore.stderr=T)
     } else {
-       execGRASS("v.in.ogr",
-              flags = c("overwrite", "quiet", "r"),
-              parameters = list(
-                input = streams,
-                output = "streams_o"),ignore.stderr=T)
+      # execGRASS("v.in.ogr",
+      #           flags = c("overwrite", "quiet", "r"),
+      #           parameters = list(
+      #             input = streams,
+      #             output = "streams_o"),ignore.stderr=T)
+      execGRASS("v.import",
+                flags = c("overwrite", "quiet"),
+                parameters = list(
+                  input = streams,
+                  output = "streams_o",
+                  extent = "region"))
     }
     # MiKatt: snapp line ends to next vertext to prevent loose ends/ unconnected streams and to build topography
     if(snap_streams){
--- R/setup_grass_environment.R
+++ R/setup_grass_environment.R
@@ -2,24 +2,12 @@
 #'
 #' This function sets the 'GRASS' mapset to PERMANENT and sets its projection and extension.
 #'
-#' @param dem character; path to DEM.
-#' @param sites character string or object; path to sites vector file (shape) 
-#' or sp data object.
-#' @param epsg number; EPSG code for the spatial reference to be used
-#' @param proj4 (optional) proj4 string; character string of projection arguments
-#'  
+#' @param dem character; path to DEM.  
 #'
-#' @return Nothing, the 'GRASS' mapset is set to PERMANENT,
-#' projection is set to the one of the sites shape, to the proj4 string or to 
-#' the epsg code provided, the extent of the region is set to the one of bounding
-#' box of the dem.
+#' @return Nothing, the 'GRASS' mapset is set to PERMANENT, the projection and
+#' the extent of the current location is set to the one of the dem.
 #'
-#' @note Either \code{sites}, \code{epsg} or \code{proj4} must be provided. Make
-#' sure that all raster and vector files are in the same projection; it will be
-#' overwritten without warning. A 'GRASS' session must be initiated before, see
-#' \code{\link[rgrass7]{initGRASS}}.
-#' If the SQLite driver is not available on the system, the extent is set by
-#' reading in the DEM temporarily; this might take a while for large files.
+#' @note A 'GRASS' session must be initiated before, see \code{\link[rgrass7]{initGRASS}}.
 #' 
 #' @author Mira Kattwinkel, \email{mira.kattwinkel@@gmx.net}
 #' @export
@@ -39,127 +27,51 @@
 #' # Load files into GRASS
 #' dem_path <- system.file("extdata", "nc", "elev_ned_30m.tif", package = "openSTARS")
 #' sites_path <- system.file("extdata", "nc", "sites_nc.shp", package = "openSTARS")
-#' setup_grass_environment(dem = dem_path, sites = sites_path)
+#' setup_grass_environment(dem = dem_path)
 #' gmeta()
 #' }
 
-setup_grass_environment <- function(dem, sites = NULL, epsg = NULL, proj4 = NULL) {
+setup_grass_environment <- function(dem) {
   if (nchar(get.GIS_LOCK()) == 0)
     stop("GRASS not initialised. Please run initGRASS().")
-  if (is.null(dem) | (is.null(sites) & is.null(epsg) & is.null(proj4)))
-    stop("DEM and either sites, epsg code or porj4 string are needed.")
   message("Setting up GRASS Environment...\n")
-
-  # Set Projection to input file -------------------------
-  ## AS: Habe optionen hinzugefügt, dass Punkte auch SP*-Objekte aus R sein können
-  execGRASS("g.mapset", flags = c("quiet"),
-            parameters = list(
-              mapset = "PERMANENT"))
-  # MiKatt: shortend
-  if(is.null(proj4)){
-    if(inherits(sites, 'Spatial')) {
-      proj4 <- sp::proj4string(sites)
-    } ## MiKatt: exclude as long as under development
-    # else if(inherits(sites, 'sf')) {
-    #     ## AS: syntax might change in future
-    #     proj4 <- sf::st_crs(sites)$proj4string
-    # }
-  }
-  if(!is.null(proj4)){
-    execGRASS("g.proj", flags = c("c"),
-              parameters = list(
-                proj4 = proj4
-              ))
-  } else if(!is.null(sites)){
-    execGRASS("g.proj", flags = c("c"),
-              parameters = list(
-                georef = sites
-              ))
-  } else {
-    execGRASS("g.proj",
-              flags = c("c"),#, "quiet"),
-              parameters = list(
-                epsg = epsg
-              ))
-    # set proj4 from epsg for dem projection
-    proj4 <- paste0("+init=epsg:", epsg)
-  }
-
-  # set Region -----------------
   
-  # read raster to set region
-  # MiKatt: flag "o": Override projection check. Necessary for Windows, otherwise DEM is not imported
-  # MiKatt: it is necassary to set the region with g.region; using flag "e" when importing the dem does not work
-  #         (r.hydrodem produces very huge raster)
-  # if(.Platform$OS.type == "windows"){
-  #   execGRASS("r.in.gdal",
-  #             flags = c("overwrite","quiet","o"),
-  #             parameters = list(
-  #               input = dem,
-  #               output = "dem_temp"))
-  #   message(paste("Windows does not recognize the projection of the DEM raster. ",
-  #                 "It will be overwritten with the one of the observation sites (",
-  #                 sites, "). Please verify that the projections match.", sep= ""))
-  # } else {
-  #   execGRASS("r.in.gdal",
-  #             flags = c("overwrite","quiet"),
-  #             parameters = list(
-  #               input = dem,
-  #               output = "dem_temp"))
-  # }
-  
-  ##AS: Anmerkungen: Windows-unix query wäre hier wahrscheinlich nicht mehr nötig?
-  ##AS: Wenn doch, müsste man sie über writeVECT wrapen
-  ##AS: Zusätzlich werden jetzt das raster u sp package verwendet, aber das sind eh standards
-  # MiKatt changed to more readable names
   dem_raster <- raster::raster(dem)
+  dem_proj <- raster::projection(dem_raster)
   dem_extent <- raster::extent(dem_raster)
   dem_extent <- as(dem_extent, 'SpatialPolygons')
   dem_extent <- SpatialPolygonsDataFrame(dem_extent, data.frame(ID = 1))
-  dem_res_x <- as.character(raster::xres(dem_raster))
-  dem_res_y <- as.character(raster::yres(dem_raster))
-  if(round(as.numeric(dem_res_x),10) != round(as.numeric(dem_res_y),10))
+  dem_res_x <- raster::xres(dem_raster)
+  dem_res_y <- raster::yres(dem_raster)
+  if(round(dem_res_x,10) != round(dem_res_y,10))
     warning("North-south and east-west resolution of dem differ. Please check!")
-  if (is.null(proj4)) {
-    raster::projection(dem_extent) <- raster::projection(raster::raster(dem))
-  } else {
-    raster::projection(dem_extent) <- sp::CRS(proj4)
-  }
+  dem_west <- dem_extent@bbox["x","min"]
+  dem_east <- dem_extent@bbox["x","max"]
+  dem_south <- dem_extent@bbox["y","min"]
+  dem_north <- dem_extent@bbox["y","max"]
+  # otherwise there might be strange cell sizes as extent seems to have priority
+  remainder_x <- (dem_north - dem_south) %% dem_res_x
+  if(remainder_x != 0)
+    dem_south <- dem_south - (dem_res_x - remainder_x)
+  remainder_y <- (dem_west - dem_east) %% dem_res_y
+  if(remainder_y != 0)
+    dem_west <- dem_west - (dem_res_y - remainder_y)
   
-  # write bounding box of dem
-  # To catch Error if driver is not installed.
-  # TODO: with GRASS 7.4 SQLite driver is no longer available!
-  test <- try(writeVECT(SDF = dem_extent, vname = 'bbox_dem', driver = 'SQLite',
-            v.in.ogr_flags = c("overwrite", "quiet")), silent = T)
-  if(class(test) != "try-error"){
-  execGRASS("g.region", flags = c("quiet"),
+  execGRASS("g.mapset", flags = c("quiet"),
             parameters = list(
-              vector = "bbox_dem",
-              nsres = dem_res_y,
-              ewres = dem_res_x
-              ))
-    # remove temporary dem file
-    execGRASS("g.remove", flags = c("quiet", "f"),
-              parameters = list(
-                type = "vector",
-                name = "bbox_dem"
-              ))
-  } else {
-    execGRASS("g.region", flags = c("quiet"),
-              parameters = list(
-                nsres = dem_res_y,
-                ewres = dem_res_x
-              ))
-    execGRASS("r.in.gdal", flags = c("overwrite","quiet","e"),
-                parameters = list(
-                  input = dem,
-                  output = "dem_temp"))
-    # remove temporary dem file
-    execGRASS("g.remove", flags = c("quiet", "f"),
-              parameters = list(
-                type = "raster",
-                name = "dem_temp"
-              ))
-  }
-
+              mapset = "PERMANENT"))
+  # g.proj must be executed before g.region, otherwise cell sizes etc. are overwritten
+  execGRASS("g.proj", flags = c("c"),
+            parameters = list(
+              proj4 = dem_proj
+            ))
+  execGRASS("g.region", flags = c("verbose"),
+            parameters = list(
+              nsres = as.character(dem_res_y),
+              ewres = as.character(dem_res_x),
+              n = as.character(dem_north),
+              s = as.character(dem_south),
+              e = as.character(dem_east),
+              w = as.character(dem_west)
+            ))
 }
