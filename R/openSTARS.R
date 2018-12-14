d5e76f2350a6af41855259ece937232e3842c716
MiKatt <kattwinkel-mira@uni-landau.de>
2018-12-14 16:13:08 +0100

    test work laptop 2

--- R/openSTARS.R
+++ R/openSTARS.R
@@ -5,7 +5,6 @@
 #' 'GRASS GIS 7.0' (or greater) with installed addons r.stream.basins,
 #' r.stream.distance, r.stream.order, and r.hydrodem is needed.
 #'
-#' # a small test comment 
 #' 
 #' @import rgrass7
 #' @import data.table
