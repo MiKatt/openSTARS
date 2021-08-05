235afa576b8b6045d556904e709b2e762d832837
MiKatt <kattwinkel-mira@uni-landau.de>
2021-08-05 18:04:28 +0200

    delete comment

--- R/correct_compl_confluences.R
+++ R/correct_compl_confluences.R
@@ -330,9 +330,6 @@ correct_compl_confluences <- function(clean = TRUE){
     streams <- readVECT(vname = "streams_v", remove.duplicates = FALSE, 
                         ignore.stderr = TRUE, type = "line")
     sink()
-    # MiKatt 20190416: not needed any more, only for debugging
-    #streams@data <- data.frame(streams@data, streams$cat)
-    #colnames(streams@data)[ncol(streams@data)] <- paste0("cat_old", i) 
 
     nstream <- max(streams$stream) + 1
     streams$str_new <- streams$stream
